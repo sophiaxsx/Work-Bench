1891d46f3524d18ea2027b546263c48ff4bbaa3b
sophiaxsx <35610465+sophiaxsx@users.noreply.github.com>
2018-10-16 16:34:19 -0400

    Update account.php

--- account.php
+++ account.php
@@ -3,6 +3,6 @@
 $hostname = "sql2.njit.edu"     ; 			// or "sql2.njit.edu"   OR "SQL1.NJIT.EDU"
 $username = "sas238" ;
 $project  = "sas238" ;
-$password = "EVhodUUSj";
+$password = "PASSWORD";
 
 ?>
